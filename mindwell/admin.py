14017c42b373e13f47c6a94bb5e95e17e6f26f90
Gracious <gpoa@bu.edu>
2025-11-25 11:26:43 -0500

    completed models

--- mindwell/admin.py
+++ mindwell/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from mindwell.models import *
 
 # Register your models here.
+admin.site.register(HealthProvider)
+admin.site.register(Patient)
+admin.site.register(TherapyPlan)    
+admin.site.register(Session)
+admin.site.register(Availability)
+admin.site.register(PlanType)
